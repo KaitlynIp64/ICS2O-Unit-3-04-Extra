6db4de5a8db3716e142ec857577c95d9fb069ff7
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-07 05:40:52 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,32 @@
+// Created by: Kaitlyn Ip
+// Created on: Apr 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        double fahrenheit;
+        double celsius;
+        //input
+        Console.WriteLine("This program converts fahrenheit to celsius.");
+        Console.WriteLine("");
+        Console.WriteLine("Formula = C = (a°F - 32) x 5/9");
+        Console.WriteLine("");
+        Console.Write("Enter Fahrenheit : ");
+        fahrenheit = Convert.ToInt32(Console.ReadLine());
+
+        //process
+        celsius = ((fahrenheit - 32) * 5.0 / 9.0);
+
+        //output
+        Console.WriteLine("");
+        Console.Write("The temperature in Celsius is: " + celsius.ToString("0.00") + " °C");
+        Console.ReadKey();
+        Console.WriteLine("\nDone.");
+    }
+}
